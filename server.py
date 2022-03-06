d1f11840d9e881a526e40df0d2bd57805fd69274
Lulky <lukyluxiano.12@gmail.com>
2022-03-06 17:43:02 -0500

    changes

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
+import datetime as dt
+
 app = Flask(__name__)  
 
 @app.route('/')         
@@ -8,11 +10,24 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    nombre = request.form['first_name']
+    apellido = request.form['last_name'] 
+    id = request.form['student_id']
+    strawberry = request.form['strawberry']
+    raspberry = request.form['raspberry']
+    apple = request.form['apple']
+    suma = int(request.form['strawberry']) + int(request.form['raspberry']) + int(request.form['apple'])
+    current_datetime=dt.datetime.now()
+    string_date = current_datetime.strftime("%A, %B %d, %Y")
+    print(f'Cobrando a {nombre} {apellido} por {suma} frutas')
+    
+    return render_template("checkout.html", nombre=nombre, apellido=apellido, id=id, apple = apple, raspberry =raspberry, strawberry=strawberry, suma=suma, string_date=string_date )
 
 @app.route('/fruits')         
 def fruits():
     return render_template("fruits.html")
+
+
 
 if __name__=="__main__":   
     app.run(debug=True)    
